1f67b877bf69592182dcc50fb47a99339b174cac
HK Kahng <hk@router-rooter.com>
2016-09-23 10:11:56 -0700

    update twig and silex to accommodate edit and delete of Stylist

--- app/app.php
+++ app/app.php
@@ -1,8 +1,8 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
-    require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Stylist.php";
-    require_once __DIR__."/../src/Client.php";
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/Stylist.php';
+    require_once __DIR__.'/../src/Client.php';
 
     $app = new Silex\Application();
     $app['debug'] = true;
@@ -17,14 +17,36 @@
     use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
 
-    $app->get("/", function() use ($app) {
+    $app->get('/', function() use ($app) {
         return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
     });
 
-    $app->post("/stylists", function() use ($app) {
+    $app->post('/stylists', function() use ($app) {
         $new_name = $_POST['stylist_name'];
         $new_stylist = new Stylist($new_name, null);
         $new_stylist->save();
+        return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+    $app->get('/stylists/{id}', function($id) use ($app) {
+        return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+    $app->get('/stylists/{id}/edit', function($id) use ($app) {
+        $found_stylist = Stylist::find($id);
+        return $app['twig']->render('stylists_edit.html.twig', array('stylist' => $found_stylist));
+    });
+
+    $app->patch('/stylists/{id}', function($id) use ($app) {
+        $new_name = $_POST['new_stylist_name'];
+        $stylist = Stylist::find($id);
+        $stylist->update($new_name);
+        return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+    $app->delete('/stylists/{id}', function($id) use ($app){
+        $stylist = Stylist::find($id);
+        $stylist->delete();
         return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
     });
 
